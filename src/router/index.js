047acc8fa6ce0aa53accb6c41729ac909a4cb94c
zphtown <zphtown@163.com>
2019-06-22 10:47:53 +0800

    '1

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexListTest from '@/components/IndexListTest'
-import BugList from '@/components/BugList'
+// import BugList from '@/components/BugList'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'BugList',
+    //   component: BugList
+    // },
     {
       path: '/',
-      name: 'BugList',
-      component: BugList
-    },
-    {
-      path: '/IndexListTest',
       name: 'IndexListTest',
       component: IndexListTest
     }
